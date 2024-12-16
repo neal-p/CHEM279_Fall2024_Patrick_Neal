df932cde1f06009e9dca95c7e534bdb638d57ed3
Patrick Neal <patrick_neal@berkeley.edu>
2024-12-16 13:31:30 -0500

    version used for timings

---
+++ final/model/simulation_speed.py
@@ -0,0 +1,125 @@
+import sys
+from pathlib import Path
+sys.path.insert(0, str((Path(".") / "utils").parent.resolve()))
+sys.path.insert(0, str((Path(".") / "model").parent.resolve()))
+
+from utils import parsers 
+from model import prediction, featurize
+import numpy as np
+import pickle
+import time
+from matplotlib import pyplot as plt
+import pandas as pd
+
+
+print(str(Path(".").resolve()))
+print(list(Path(".").glob("*")))
+print(list(Path("results").glob("*")))
+
+
+def simulation(coords, num_steps, predictor):
+
+    # all_coords = [coords.copy()]
+    # all_energies = []
+    # h1_distances = []
+    # h2_distances = []
+    # angles = []
+
+    energy, forces = predictor.predict(coords)
+    # all_energies.append(energy)
+
+    forces_norm = np.linalg.norm(forces)
+
+    for step in range(num_steps):
+
+        randomness = np.random.normal(0, 1, (3, 3))
+        randomness_norm = np.linalg.norm(randomness)
+        scaler = 0.5 * forces_norm / randomness_norm
+        randomness *= scaler
+        
+        H1_O = coords[1] - coords[0]
+        bd1 = np.linalg.norm(H1_O)
+        H2_O = coords[2] - coords[0]
+        bd2 = np.linalg.norm(H2_O)
+
+        if bd1 > 2.0:
+            correction = (bd1 - 2.0) * (H1_O / bd1)  # Adjust distance to 2.0
+            coords[1] -= correction / 2  # Move H1 slightly
+            coords[0] += correction / 2  # Move O slightly to conserve COM
+
+        if bd2 > 2.0:
+            correction = (bd2 - 2.0) * (H2_O / bd2)  # Adjust distance to 2.0
+            coords[2] -= correction / 2  # Move H2 slightly
+            coords[0] += correction / 2  # Move O slightly to conserve COM
+
+        coords = coords + 0.01 * (forces + randomness)
+        energy, forces = predictor.predict(coords)
+
+        # all_energies.append(energy)
+        # all_coords.append(coords.copy())
+        # h1_distances.append(bd1)
+        # h2_distances.append(bd2)
+        # angles.append(np.degrees(featurize.angle(H1, O, H2)))
+
+    return #all_coords, all_energies, h1_distances, h2_distances, angles
+
+
+
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        print("ERROR")
+        print("Provide starting structure")
+        exit()
+
+    trained_model_dir = Path("model_dir")
+    initial_geometry = Path(sys.argv[1])
+
+    elements, coords = parsers.read_xyz(initial_geometry)
+    coords *= 1.8897 # convert to Bohr
+    masses = np.array([parsers.Element(e).getMass() for e in elements])  # in AMU
+
+    time_step = 0.5 # femtoseconds
+    num_steps = 5000 # number of steps
+
+    predictor = prediction.Predictor(trained_model_dir)
+
+    start = time.time()
+    simulation(coords, num_steps, predictor)
+    end = time.time()
+
+    # traj = [c / 1.8897 for c in traj]  # convert back to angstrom
+    # parsers.write_multi_xyz(elements, traj, f"results/traj.xyz")
+
+    # plt.plot(list(range(len(energies))), energies)
+    # plt.title("Simulation Energy")
+    # plt.xlabel("Step")
+    # plt.ylabel("Energy (eV)")
+    # plt.savefig(f"results/traj_energy.png")
+    # plt.clf()
+
+    # plt.plot(list(range(len(h1_distances))), h1_distances, label="H1-O")
+    # plt.plot(list(range(len(h2_distances))), h2_distances, label="H2-O")
+    # plt.title("Simulation Distances")
+    # plt.xlabel("Step")
+    # plt.ylabel("H-O Distance (Bohr)")
+    # plt.legend()
+    # plt.savefig(f"results/traj_distances.png")
+    # plt.clf()
+
+    # plt.plot(list(range(len(angles))), angles)
+    # plt.title("Simulation Angles")
+    # plt.xlabel("Step")
+    # plt.ylabel("Angles (degrees)")
+    # plt.savefig(f"results/traj_angles.png")
+    # plt.clf()
+
+    # 
+    # df = pd.DataFrame(list(zip(energies, h1_distances, h2_distances, angles)), columns=["energy", "H1-O_distance", "H2-O_distance", "H1-O-H2_angle"])
+    # df.to_csv("traj_info.csv")
+
+    print(end - start)
+
+
